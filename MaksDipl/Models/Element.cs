d103af25b49cb829412ab212824ef48bc4896dc5
Tim Popov <popov_ta@mail.ru>
2020-11-05 11:19:32 +0300

    Начало

---
+++ MaksDipl/Models/Element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace MaksDipl.Models
+{
+   public class Element
+   {
+       public Guid Id { get; set; }
+       public Point Location { get; set; }
+       public string Value { get; set; }
+
+
+   }
+}
